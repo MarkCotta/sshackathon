694ed1607908172ef0a2bbe1cfef770387c0de28
minsaraga <123310893+minsaraga@users.noreply.github.com>
2023-03-23 13:26:15 +0530

    Update register.php

--- register.php
+++ register.php
@@ -52,10 +52,13 @@
 <html lang="en">
     <head>
         <title>SafeSpend | Register</title>
-        <link rel="stylesheet" href="bootstrap2/css/bootstrap.min.css" type="text/css">
-        <script type="text/javascript" src="bootstrap2/js/jquery-3.5.1.min.js"></script>
-        <script type="text/javascript" src="bootstrap2/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="css2/loginstyle.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <link rel="stylesheet" href="bootstrap/fonts/bootstrap-icons.css" type="text/css">
+        
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script type="text/javascript" src="bootstrap/js/jquery-3.6.1.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
             body{
