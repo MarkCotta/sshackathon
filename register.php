41997708c89a5397ad81e0fab8f12db4d551e658
MarkCotta <69809600+MarkCotta@users.noreply.github.com>
2023-03-24 02:32:24 +0530

    Revert "for hosting"
    This reverts commit 07bfad66e936cc577cd4b6680d9b98cb07d6552a.

--- register.php
+++ register.php
@@ -119,16 +119,6 @@
                     </div>
                 </div>
             </div>
-            <script>
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then(function(registration) {
-      console.log('Service worker registered:', registration);
-    })
-    .catch(function(error) {
-      console.log('Service worker registration failed:', error);
-    });
-}
-</script>
+        
     </body>
 </html> 
