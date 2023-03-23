47b02bb50d243a95e11e4d5c4b4266008ac1c903
Kaustubh Athalekar <kaustubhdva@gmail.com>
2023-03-23 13:29:48 +0530

    Budget Pie Charts & format code

--- report-html/index1.php
+++ report-html/index1.php
@@ -1,6 +1,6 @@
-<?php session_start(); 
-if(!isset($_SESSION['user_name']))
-header("Location:../login.php");
+<?php session_start();
+if (!isset($_SESSION['user_name']))
+    header("Location:../login.php");
 ?>
 <html>
 <title>SafeSpend | Transactions</title>
@@ -16,47 +16,56 @@ header("Location:../login.php");
     <link rel="stylesheet" href="index1.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <script src="https://kit.fontawesome.com/2291efdc8d.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    
     <style>
-    .progressbar{
-        border-radius: 10px;
-        color:black;
-        background: greenyellow;
-    }
-    .Progress p{
-        font-size: 13;
-    }
-    button{
-            cursor:pointer;
+        .progressbar {
+            border-radius: 10px;
+            color: black;
+            background: greenyellow;
         }
-    .menu-button1 {
-  width: 130px;
-  height: 40px;
-  background-color: #aeff83;
-  border-radius: 10px;
-  border-color: #f5f5fb;
-  transition: 0.3s;
-  align-items: center;
-}
-.menu-button1:hover{
-    background-color: greenyellow;
-    width:140px;
-    height:45px;
-}
-.add-transaction-button{
-  background-color: #4ad400;
-  width: 200px; 
-  height:40px;
-  font-weight: 700;
-  font-size: large;
-  font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-  transition: 0.3s;
-}
-.add-transaction-button:hover{
-    background-color: darkgreen;
-    width:210px;
-    height:45px;
-}
-</style>
+
+        .Progress p {
+            font-size: 13;
+        }
+
+        button {
+            cursor: pointer;
+        }
+
+        .menu-button1 {
+            width: 130px;
+            height: 40px;
+            background-color: #aeff83;
+            border-radius: 10px;
+            border-color: #f5f5fb;
+            transition: 0.3s;
+            align-items: center;
+        }
+
+        .menu-button1:hover {
+            background-color: greenyellow;
+            width: 140px;
+            height: 45px;
+        }
+
+        .add-transaction-button {
+            background-color: #4ad400;
+            width: 200px;
+            height: 40px;
+            font-weight: 700;
+            font-size: large;
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+            transition: 0.3s;
+        }
+
+        .add-transaction-button:hover {
+            background-color: darkgreen;
+            width: 210px;
+            height: 45px;
+        }
+    </style>
     <!---->
 
 </head>
@@ -82,10 +91,11 @@ header("Location:../login.php");
                         while ($images = mysqli_fetch_assoc($result)) {
 
 
-                ?>
-                <img src="../uploads/<?= $images['profile_pic_url'] ?>" class="profilephoto1" style="width: 50px; border:solid black;border-radius:25px;position:relative;top:15px;">
+                            ?>
+                            <img src="../uploads/<?= $images['profile_pic_url'] ?>" class="profilephoto1"
+                                style="width: 50px; border:solid black;border-radius:25px;position:relative;top:15px;">
 
-                <?php
+                            <?php
                         }
                     }
                 }
@@ -106,21 +116,20 @@ header("Location:../login.php");
                 $var = $_SESSION['user_name'];
                 $sql = "SELECT * from Budget join keeps on BID=Budget_ID where emailkeeps='$var'";
                 $result = mysqli_query($conn, $sql);
-                if(mysqli_num_rows($result) > 0){
-                    while( $row = mysqli_fetch_assoc($result)){
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
                         $spentamt = $row['Spent_amount'];
                         $totalamt = $row['Total_amount'];
                         $category = $row['category'];
-                        if($totalamt!=0)
-                        $percentage = (float) $spentamt / (float) $totalamt * 100;
+                        if ($totalamt != 0)
+                            $percentage = (float) $spentamt / (float) $totalamt * 100;
                         else
                             $percentage = 0;
                         $percentage = round($percentage, 2);
-                        if($spentamt>$totalamt)
-                        {
-                            echo '<p style="color:red;">Budget_for_'.$category.'_exceeded '.$percentage.'% Used</p>';
+                        if ($spentamt > $totalamt) {
+                            echo '<p style="color:red;">Budget_for_' . $category . '_exceeded ' . $percentage . '% Used</p>';
                         } else {
-                            echo  '<p style="color:green;"> '. $row['category'] .'<br>'. $percentage .'%<br>';
+                            echo '<p style="color:green;"> ' . $row['category'] . '<br>' . $percentage . '%<br>';
                             echo '<progress class="progressbar" value="' . $spentamt . '" max="' . $totalamt . '"></progress>';
                         }
                     }
@@ -153,12 +162,11 @@ header("Location:../login.php");
             </div>
             <div class="reminders-page-left-menu">
                 <span class="reminders-page-text1"><a href="../transactions-html/index1.php"><button
-                            class="menu-button1" ><span
-                                class="menutext">Transactions</span></button></a></span>
+                            class="menu-button1"><span class="menutext">Transactions</span></button></a></span>
                 <span class="reminders-page-text2"><a href="../budget-html/index1.php"><button
                             class="menu-button1"><span class="menutext">Budgets</span></button></a></span>
-                <span class="reminders-page-text3"><a href="../report-html/index1.php"><button
-                            class="menu-button1" style="background-color:#65f9c5;"><span class="menutext">Report</span></button></a></span>
+                <span class="reminders-page-text3"><a href="../report-html/index1.php"><button class="menu-button1"
+                            style="background-color:#65f9c5;"><span class="menutext">Report</span></button></a></span>
                 <span class="reminders-page-text4"><a href="../activities-html/index1.php"><button
                             class="menu-button1"><span class="menutext">Activities</span></button></a></span>
                 <span class="reminders-page-text5"><a href="../reminders-html/index1.php"><button
@@ -169,14 +177,14 @@ header("Location:../login.php");
                             class="menu-button1"><span class="menutext">Educate</span></button></a></span>
                 <span class="reminders-page-text7"><a href="../help-html/index1.php"><button class="menu-button1"><span
                                 class="menutext">Help</span></button></a></span>
-                                <span class="reminders-page-text8"><a href="../shopping-html/index1.php"><button class="menu-button1"><span
-                                class="menutext">Shopping List</span></button></a></span>
+                <span class="reminders-page-text8"><a href="../shopping-html/index1.php"><button
+                            class="menu-button1"><span class="menutext">Shopping List</span></button></a></span>
 
                 <div style="position: relative; top:670px; left:20px;"><a href="../register.php"><span
                             style="color: red; font-size: 20; ">Log Out</span></a></div>
             </div>
         </div>
-         <div class="searchbar">
+        <div class="searchbar">
             <form>
                 <input type="text" class="" id="myInput" style="border: 20px;width:160px;" onkeyup="myFunction()"
                     placeholder="Search for a Category...">
@@ -184,9 +192,9 @@ header("Location:../login.php");
             <img src="../public/playground_assets/search.svg" class="Searchicon">
         </div>
         <!-- Mid Division-->
-        
+
         <div class="reminders-page-middle">
-        <h1>Report</h1>
+            <h1>Report</h1>
             <?php
             $mysqli = new mysqli('localhost', 'root', '', 'safespend-2');
             $var = $_SESSION['user_name'];
@@ -206,18 +214,18 @@ header("Location:../login.php");
                     $array[$category] = $row;
                 }
                 //IF You add new categories, add it here
-
+            
                 $reqarray = array($array["General"]['total_amount'], $array["Transport"]['total_amount'], $array["Food"]['total_amount'], $array["Shopping"]['total_amount'], $array["Rent"]['total_amount'], $array["Petrol"]['total_amount'], $array["Medicine"]['total_amount'], $array["Entertainment"]['total_amount']);
-                
+
             }
             ?>
             <div style="margin:auto;">
                 <canvas id="myChart" style="width:100%; max-width:600px;"></canvas>
                 <script>
-                    var xValues = ["General", "Transport", "Food","Shopping","Rent","Petrol","Medicine","Entertainment"];
+                    var xValues = ["General", "Transport", "Food", "Shopping", "Rent", "Petrol", "Medicine", "Entertainment"];
 
-                    var yValues = <?php echo '["' .implode('", "', $reqarray). '"]' ?>;
-                    var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145", "#f7de3f", "#FF5733", "#FFC300" ];
+                    var yValues = <?php echo '["' . implode('", "', $reqarray) . '"]' ?>;
+                    var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145", "#f7de3f", "#FF5733", "#FFC300"];
                     new Chart("myChart", {
                         type: "pie",
                         data: {
@@ -244,30 +252,30 @@ header("Location:../login.php");
 
                 <canvas id="myChart2" style="width:100%;max-width:600px"></canvas>
                 <?php
-            $array = array();
-            $query = "SELECT Date, sum(amount) as total_amount FROM transaction join performs on TID = Transaction_ID where Emailperforms = '$var' and Type = 'Debit' group by Date ";
-            if ($result = $mysqli->query($query)) {
-                while ($row = $result->fetch_assoc()) {
-                    $array[] = $row;
+                $array = array();
+                $query = "SELECT Date, sum(amount) as total_amount FROM transaction join performs on TID = Transaction_ID where Emailperforms = '$var' and Type = 'Debit' group by Date ";
+                if ($result = $mysqli->query($query)) {
+                    while ($row = $result->fetch_assoc()) {
+                        $array[] = $row;
+                    }
                 }
-            }
-            ?>
+                ?>
                 <script>
-                    var xValues = 
-                    <?php 
-                    $i = 0;
-                    for ($i = 0; $i < count($array) ; $i++)
-                        $reqarray1[$i] = $array[$i]['Date'];
-                    echo '["' .implode('", "', $reqarray1). '"]';
-                    ?>;
+                    var xValues =
+                        <?php
+                        $i = 0;
+                        for ($i = 0; $i < count($array); $i++)
+                            $reqarray1[$i] = $array[$i]['Date'];
+                        echo '["' . implode('", "', $reqarray1) . '"]';
+                        ?>;
 
-                    var yValues = 
-                    <?php 
-                    $i = 0;
-                    for ($i = 0; $i < count($array) ; $i++)
-                        $reqarray2[$i] = $array[$i]['total_amount'];
-                    echo '["' .implode('", "', $reqarray2). '"]';
-                    ?>;
+                    var yValues =
+                        <?php
+                        $i = 0;
+                        for ($i = 0; $i < count($array); $i++)
+                            $reqarray2[$i] = $array[$i]['total_amount'];
+                        echo '["' . implode('", "', $reqarray2) . '"]';
+                        ?>;
 
                     new Chart("myChart2", {
                         type: "line",
@@ -290,7 +298,7 @@ header("Location:../login.php");
                     });
                 </script>
             </div>
-            
+
         </div>
     </div>
 </body>
